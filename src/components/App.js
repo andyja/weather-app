777422d016f52115a779f7c0787a4898622275f2
Andy Jackson <andypauljackson@gmail.com>
2022-10-10 10:56:23 +0100

    set date added

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
@@ -7,14 +7,40 @@ import ForecastDetails from "./ForecastDetails";
 
 function App(props) {
   const { location, forecasts } = props;
+  const initialDate = forecasts[0].date;
+
+  const [selectedDate] = useState(initialDate);
+
+  const selectedForecast = forecasts.find(
+    (forecast) => forecast.date === selectedDate
+  );
+
+  console.log(selectedForecast);
+
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
       <ForecastSummaries forecasts={forecasts} />
-      <ForecastDetails forecast={forecasts[1]} />
+      <ForecastDetails forecast={forecasts[0]} />
     </div>
   );
 }
+
+// const App = ({ forecasts, location }) => {
+//   const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
+//   const selectedForecast = forecasts.find(forecast => forecast.date === selectedDate);
+
+//   return (
+//     <div className="weather-app">
+//       <LocationDetails city={location.city} country={location.country} />
+//       <ForecastSummaries forecasts={forecasts} />
+//       <ForecastDetails forecast={forecasts[0]} />
+//     </div>
+//   )
+// }
+
+// function App(props) {
+//   const { location, forecasts } = props;
 
 App.propTypes = {
   forecasts: PropTypes.arrayOf(
